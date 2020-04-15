acc838bbc25eb42e21c05f05b61350c1b68e37e0
jondo <slim.shady99q@gmail.com>
2020-04-15 08:17:28 -0500

    Metodos que pasan los datos separados por arrays

--- Parcial/src/co/edu/unbosque/controller/Controller.java
+++ Parcial/src/co/edu/unbosque/controller/Controller.java
@@ -20,21 +20,21 @@ import co.edu.unbosque.model.Estudiante;
  */
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Estudiante> estudiantes;
 	private DTO dto;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        estudiantes = new ArrayList<Estudiante>();
-        dto = new DTO();
-        dto.CargarEstudientes(estudiantes);
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Controller() {
+		super();
+		estudiantes = new ArrayList<Estudiante>();
+		dto = new DTO();
+		dto.CargarEstudientes(estudiantes);
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -48,7 +48,7 @@ public class Controller extends HttpServlet {
 		request.setAttribute("IP", ip);
 		RequestDispatcher miDis = request.getRequestDispatcher("/primera.jsp");
 		miDis.forward(request, response);
-		
+
 	}
 
 	/**
@@ -58,9 +58,63 @@ public class Controller extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
-	public ArrayList<Estudiante> estatiditcas(String filtro){
-		
+
+
+	public ArrayList<String> separarPorNombre(){
+		ArrayList<String> nombres = new ArrayList<String>();
+		for (Estudiante i : estudiantes) {
+			nombres.add(i.getNombre());
+		}
+		return nombres;
+	}
+	public ArrayList<String> separarPorEdad(){
+		ArrayList<String> nombres = new ArrayList<String>();
+		for (Estudiante i : estudiantes) {
+			nombres.add(i.getEdad());
+		}
+		return nombres;
+	}
+	public ArrayList<String> separarPorGenero(){
+		ArrayList<String> nombres = new ArrayList<String>();
+		for (Estudiante i : estudiantes) {
+			nombres.add(i.getGenero());
+		}
+		return nombres;
+	}
+	public ArrayList<String> separarPorCarrera(){
+		ArrayList<String> nombres = new ArrayList<String>();
+		for (Estudiante i : estudiantes) {
+			nombres.add(i.getCarrera());
+		}
+		return nombres;
+	}
+	public ArrayList<String> separarPorDocumento(){
+		ArrayList<String> nombres = new ArrayList<String>();
+		for (Estudiante i : estudiantes) {
+			nombres.add(i.getDocumento());
+		}
+		return nombres;
+	}
+	public ArrayList<String> separarPorSemestre(){
+		ArrayList<String> nombres = new ArrayList<String>();
+		for (Estudiante i : estudiantes) {
+			nombres.add(i.getSemestre());
+		}
+		return nombres;
+	}
+	public ArrayList<String> separarPorPromedio(){
+		ArrayList<String> nombres = new ArrayList<String>();
+		for (Estudiante i : estudiantes) {
+			nombres.add(i.getPromedio());
+		}
+		return nombres;
+	}
+	public ArrayList<String> separarPorEstado(){
+		ArrayList<String> nombres = new ArrayList<String>();
+		for (Estudiante i : estudiantes) {
+			nombres.add(i.getEstado());
+		}
+		return nombres;
 	}
 
 }
